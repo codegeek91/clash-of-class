583c3d9a223913b70320d7ac966314d45c27f6fd
codegeek1991@gmail.com <codegeek1991@gmail.com>
2022-01-27 18:38:55 -0500

    Enemy class cleaning

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,10 +1,10 @@
 #include "Enemy.h"
 
 Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture) :
-    worldPos(pos),
     texture(idle_texture),
     idle(idle_texture),
-    run(run_texture)
+    run(run_texture),
+    worldPos(pos)
 {
     width = texture.width / maxFrames;
     height = texture.height;
--- Enemy.h
+++ Enemy.h
@@ -9,11 +9,11 @@ class Enemy
         void undoMovement();
         Rectangle getCollisionRec();
     private:
-        Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
-        Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
-        Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
-        Vector2 screenPos{};
+        Texture2D texture{};
+        Texture2D idle{};
+        Texture2D run{};
         Vector2 worldPos{};
+        Vector2 screenPos{};
         Vector2 worldPosLastFrame{};
         // 1: facing right, -1: facing left
         float rightLeft {1.f};
