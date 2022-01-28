7bda2feb8addd5cdeb89289bdea04bed35935744
codegeek1991@gmail.com <codegeek1991@gmail.com>
2022-01-27 22:33:42 -0500

    Adding basic IA to enemy class for chasing player

--- Enemy.cpp
+++ Enemy.cpp
@@ -9,10 +9,20 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
     worldPos = pos;
     width = texture.width / maxFrames;
     height = texture.height;
+    speed = 3.5f;
 }
 
 void Enemy::tick(float deltaTime)
 {
+    // calculate vector for enemy movement and normalize
+    Vector2 toTarget = Vector2Normalize(Vector2Subtract(target -> getScreenPos(), screenPos));
+
+    // adding speed
+    toTarget = Vector2Scale(toTarget, speed);
+
+    // moving enemy
+    worldPos = Vector2Add(worldPos, toTarget);
+
     screenPos = Vector2Subtract(worldPos, target -> getWorldPos());
     BaseCharacter::tick(deltaTime);
 }
