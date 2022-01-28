33f44a181b2f9020f506f288acd65c9e72cb6ff3
codegeek1991@gmail.com <codegeek1991@gmail.com>
2022-01-27 22:29:44 -0500

    Added public method to get character screen position

--- Character.h
+++ Character.h
@@ -9,6 +9,7 @@ class Character : public BaseCharacter
     public:
         Character(int windowWidth, int windowHeight);
         virtual void tick(float deltaTime) override;
+        Vector2 getScreenPos() {return screenPos;};
     private:
 };
 
