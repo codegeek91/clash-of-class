2ec731a09a4e14fde76bb3b103b786bdcbbf34f7
codegeek1991@gmail.com <codegeek1991@gmail.com>
2022-01-27 21:53:56 -0500

    Refactoring Character and Enemy Classes into BaseCharacter Class

--- Character.cpp
+++ Character.cpp
@@ -13,8 +13,7 @@ Character::Character(int windowWidth, int windowHeight)
 
 void Character::tick(float deltaTime)
 {
-    //set worldpos last frame
-    worldPosLastFrame = worldPos;
+    BaseCharacter::tick(deltaTime);
 
     Vector2 direction{};
     if (IsKeyDown(KEY_A)) direction.x -= 1.0;
@@ -33,32 +32,4 @@ void Character::tick(float deltaTime)
     {
         texture = idle;
     }
-
-    // update animation frame
-    runningTime += deltaTime;
-    if(runningTime >= updateTime)
-    {
-        frame++;
-        runningTime = 0.f;
-        if (frame > maxFrames) frame = 0;
-    }
-
-    // draw character
-    Rectangle source{width * frame, 0.f, rightLeft * width, height};
-    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
-    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
-}
-
-void Character::undoMovement()
-{
-    worldPos = worldPosLastFrame;
-}
-
-Rectangle Character::getCollisionRec()
-{
-    return Rectangle{
-        screenPos.x,
-        screenPos.y,
-        width * scale,
-        height * scale};
 }
--- Enemy.h
+++ Enemy.h
@@ -1,29 +1,10 @@
 #include "raylib.h"
+#include "BaseCharacter.h"
 
-class Enemy
+class Enemy : public BaseCharacter
 {
     public:
         Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture);
-        Vector2 getWorldPos() {return worldPos;}
-        void tick(float deltaTime);
-        void undoMovement();
-        Rectangle getCollisionRec();
+        virtual void tick(float deltaTime) override;
     private:
-        Texture2D texture{};
-        Texture2D idle{};
-        Texture2D run{};
-        Vector2 worldPos{};
-        Vector2 screenPos{};
-        Vector2 worldPosLastFrame{};
-        // 1: facing right, -1: facing left
-        float rightLeft {1.f};
-        // animation variables
-        float runningTime{};
-        int frame{};
-        int maxFrames{6};
-        float updateTime{1.f/12.f};
-        float speed{4.f};
-        float width{};
-        float height{};
-        float scale{4.0f};
 };
